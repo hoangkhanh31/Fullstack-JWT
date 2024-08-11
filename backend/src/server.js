3833854fcaf86780c93bc65888d75ca6d26774ef
hoangkhanh31 <hoangkhanhdragon@gmail.com>
2024-08-12 01:41:50 +0700

    Set up CORS from backend only frontend can call API

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,6 +8,27 @@ import initWebRoutes from "./routes/web";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//Set up CORS only frontend can call API
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 //config view engine
 configViewEngine(app);
 
